9c11dda1325448e263e42da5a4d7f0b65d4fe065
Alister Lewis-Bowen <alister@different.com>
2016-10-25 08:50:37 -0400

    clean up results location

--- exp3/generate_radial_tree_svg.sh
+++ exp3/generate_radial_tree_svg.sh
@@ -4,12 +4,13 @@
 
 cmd="$1"
 
-csv="../exp2/${cmd}.csv"
-data="${cmd}.csv"
-html="${cmd}_taxo.html"
+csv="../exp2/results/${cmd}.csv"
+data="results/${cmd}.csv"
+html="results/${cmd}_taxo.html"
 ## create csv file suiatable for d3
 cat "$csv" | sed 's/,/./g' | sed 's/$/,/' | sed '1i\
 id,value\
 ' > "$data"
 ## create html file to contain diagram
-cat taxo_diagram_tmpl.html | sed "s/##FILE##/$data/g" > "$html"
+
+cat taxo_diagram_tmpl.html | sed "s/##FILE##/$(basename $data)/g" > "$html"
