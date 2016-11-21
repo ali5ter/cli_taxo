0521a09fffdc04d2d0cc9f45362523f8edf873bb
Alister Lewis-Bowen <alister@different.com>
2016-11-21 12:26:24 -0500

    update commands filter to include subcommands

--- exp1/photon_taxo.sh
+++ exp1/photon_taxo.sh
@@ -3,7 +3,7 @@
 # @author Alister Lewis-Bowen <bowena@vmware.com>
 
 ../cli_taxo.py photon \
-    --commands-token '^Available subcommands:' \
+    --commands-token '^Available subcommands' \
     --commands-filter '^\s\s\s\s(?!-)(\S+)' \
     --options-filter '^\s\s\s\s(-\S+)|\s(--\S+)' \
     "$@"
