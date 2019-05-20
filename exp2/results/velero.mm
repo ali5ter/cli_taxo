8603a4ddaa47679280d73be1b9402bc83e8bf3b9
Alister Lewis-Bowen <bowena@vmware.com>
2019-05-20 10:24:36 -0400

    Add velero

---
+++ exp2/results/velero.mm
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<map version="0.9.0">
+    <node folded="false" text="velero">
+        <node text="completion" />
+        <node folded="false" text="restic">
+            <node folded="false" text="repo">
+                <node text="get" />
+            </node>
+            <node text="server" />
+        </node>
+        <node text="help" />
+        <node folded="false" text="schedule">
+            <node text="create" />
+            <node text="describe" />
+            <node text="get" />
+            <node text="delete" />
+        </node>
+        <node folded="false" text="get">
+            <node text="backup-locations" />
+            <node text="backups" />
+            <node text="schedules" />
+            <node text="snapshot-locations" />
+            <node text="restores" />
+        </node>
+        <node folded="false" text="create">
+            <node text="backup-location" />
+            <node text="schedule" />
+            <node text="backup" />
+            <node text="restore" />
+        </node>
+        <node folded="false" text="describe">
+            <node text="restores" />
+            <node text="backups" />
+            <node text="schedules" />
+        </node>
+        <node folded="false" text="plugin">
+            <node text="add" />
+            <node text="remove" />
+        </node>
+        <node text="server" />
+        <node folded="false" text="snapshot-location">
+            <node text="create" />
+            <node text="get" />
+        </node>
+        <node folded="false" text="client">
+            <node folded="false" text="config">
+                <node text="set" />
+                <node text="get" />
+            </node>
+        </node>
+        <node folded="false" text="restore">
+            <node text="create" />
+            <node text="describe" />
+            <node text="logs" />
+            <node text="get" />
+            <node text="delete" />
+        </node>
+        <node text="version" />
+        <node text="install" />
+        <node folded="false" text="backup-location">
+            <node text="create" />
+            <node text="get" />
+        </node>
+        <node folded="false" text="backup">
+            <node text="logs" />
+            <node text="get" />
+            <node text="create" />
+            <node text="describe" />
+            <node text="download" />
+            <node text="delete" />
+        </node>
+        <node text="bug" />
+        <node folded="false" text="delete">
+            <node text="restore" />
+            <node text="backup" />
+            <node text="schedule" />
+        </node>
+    </node>
+</map>
